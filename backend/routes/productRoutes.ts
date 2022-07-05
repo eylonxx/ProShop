7f9443489023fb48e0bdbdb3d08ddc9266fa94eb
Eylon <eylon.perets@gmail.com>
2022-07-05 20:22:50 +0300

    error handler middleware

---
+++ backend/routes/productRoutes.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import Product from '../models/productModel';
+import asyncHandler from 'express-async-handler';
+
+const router = express.Router();
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+router.get(
+  '/',
+  asyncHandler(async (req: Request, res: Response) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+router.get(
+  '/:id',
+  asyncHandler(async (req: Request, res: Response) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error('Product not found');
+    }
+  })
+);
+
+export default router;
--- backend/server.ts
+++ backend/server.ts
@@ -1,21 +1,17 @@
 import express from 'express';
-import products from './data/products';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import colors from 'colors';
+import productRoutes from './routes/productRoutes';
+import { errorHandler, notFound } from './middleware/errorMiddleware';
 
 dotenv.config();
 connectDB();
 const app = express();
+app.use('/api/products', productRoutes);
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
